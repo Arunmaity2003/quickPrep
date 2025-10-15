9a540e13a0409053648a3676896e1f51bccef8ff
Arun Maity <154771392+Arunmaity2003@users.noreply.github.com>
2025-10-15 17:44:08 +0530

    first commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,21 +1,42 @@
+// import { initializeApp,getApp,getApps } from "firebase/app";
+// import { getAuth } from "firebase-admin/auth";
+// import { getFirestore } from "firebase-admin/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA5U09ixOdgjXKiN7fyfqmlBtmu1gisThk",
+//   authDomain: "quickprep-e7ca4.firebaseapp.com",
+//   projectId: "quickprep-e7ca4",
+//   storageBucket: "quickprep-e7ca4.firebasestorage.app",
+//   messagingSenderId: "278622635819",
+//   appId: "1:278622635819:web:dbf674cef602ae0041276e",
+//   measurementId: "G-T9WMF7W2JK"
+// };
+
+// // Initialize Firebase
+// const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+// export const auth = getAuth(app)
+// export const db = getFirestore(app)
+
+
+
 import { initializeApp,getApp,getApps } from "firebase/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA5U09ixOdgjXKiN7fyfqmlBtmu1gisThk",
-  authDomain: "quickprep-e7ca4.firebaseapp.com",
-  projectId: "quickprep-e7ca4",
-  storageBucket: "quickprep-e7ca4.firebasestorage.app",
-  messagingSenderId: "278622635819",
-  appId: "1:278622635819:web:dbf674cef602ae0041276e",
-  measurementId: "G-T9WMF7W2JK"
+  apiKey: "AIzaSyBaDhq1zOJ3Vd5Imj2pffrwokW1LZWQdNY",
+  authDomain: "quickprep-3bb10.firebaseapp.com",
+  projectId: "quickprep-3bb10",
+  storageBucket: "quickprep-3bb10.firebasestorage.app",
+  messagingSenderId: "877487115001",
+  appId: "1:877487115001:web:8a509557a0229e23d73028",
+  measurementId: "G-B17GNDNBME"
 };
 
-// Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
-
 export const auth = getAuth(app)
 export const db = getFirestore(app)
